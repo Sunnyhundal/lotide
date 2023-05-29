3dfe1961f19b8c6d254b94d231766a307905d588
Sunny Hundal <amandip.h@gmail.com>
2023-05-29 09:38:36 -0700

    added testing assertArraysEqual and eqArrays

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,30 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  let arrayCounter = 0;
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      arrayCounter ++;
+    }
+  }
+  
+  if (arrayCounter !== arrayOne.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
 const letterPositions = function(sentence, letter){
 const result = {};
 let letters =[];
@@ -27,4 +54,7 @@ return result;
 //create if-else statement to push index results
 
 
-console.log(letterPositions("lighthouse in the house"));
+//console.log(letterPositions("lighthouse in the house"));
+
+//test case
+assertArraysEqual(letterPositions("hello").e, [1]);
