373b3e6fd72378a496c53089edcd6f3df77853ac
Sunny Hundal <amandip.h@gmail.com>
2023-06-08 21:34:45 -0700

    Exported head to work with tailTest.js

--- tail.js
+++ tail.js
@@ -1,13 +1,7 @@
-const assertEqual = function(actual, expected) {
-  
-  if (actual.toString() === expected.toString()) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 let tail = function(assertEqual) {
   return assertEqual.slice(1);
 };
+
+module.exports = tail;
