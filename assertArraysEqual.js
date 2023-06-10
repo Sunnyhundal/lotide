6869077de03c37994296c23a1853c6b22b7672bb
Sunny Hundal <amandip.h@gmail.com>
2023-06-09 22:12:49 -0700

    Removed commented out code for eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,5 @@
 const eqArrays = require('./eqArrays');
 
-// const eqArrays = function(arrayOne, arrayTwo) {
-//   let arrayCounter = 0;
-//   if (arrayOne.length !== arrayTwo.length) {
-//     return false;
-//   }
-  
-//   for (let i = 0; i < arrayOne.length; i++) {
-//     if (arrayOne[i] === arrayTwo[i]) {
-//       arrayCounter ++;
-//     }
-//   }
-  
-//   if (arrayCounter !== arrayOne.length) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (eqArrays(arrayOne, arrayTwo)) {
     console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
