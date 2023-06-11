288d5fdb17d35aaa80bb8b8fa6c9f77f4669af00
Sunny Hundal <amandip.h@gmail.com>
2023-06-11 01:02:59 -0700

    Added comments to describe function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 //Imports eqArray to help get a deeper look at arrays
 const eqArrays = require('./eqArrays');
-
+//Function compares two arrays to assess if they are equal or diffrent
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (eqArrays(arrayOne, arrayTwo)) {
     console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
