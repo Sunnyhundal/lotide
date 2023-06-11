016f618c652966f454b99ddef714e89515567deb
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 18:18:13 -0700

    Removed old test cases and implemented Mocha & Chai for testing

---
+++ test/middleTest.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
+
+// assertArraysEqual(middle([1]),[1]); //fail
+// assertArraysEqual(middle([1, 2]),[1.5]);//fail
+
+// assertArraysEqual(middle([1, 2, 3]),[2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,19 @@
 // importing tail and assertEqual
-const tail = require('../head.js');
-const assertEqual = require('../assertEqual');
+// const tail = require('../head.js');
+// const assertEqual = require('../assertEqual');
 
 //test case
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+  it("returns Hello for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "labs");
+  })
+});
