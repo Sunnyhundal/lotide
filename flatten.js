ff2b81cf76f3deb5ff6b8971f41ee7b61a9c171c
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 23:22:14 -0700

    Remove eqArray and assertArraysEqual. Remove test code.

--- flatten.js
+++ flatten.js
@@ -1,30 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let arrayCounter = 0;
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] === arrayTwo[i]) {
-      arrayCounter ++;
-    }
-  }
-  
-  if (arrayCounter !== arrayOne.length) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${arrayOne} !== ${arrayTwo}`);
-  }
-};
-
 const flattenTwo = function(array, arr){
   newArray =[]
   for (let i = 0; i < array.length; i++) {
@@ -40,12 +13,4 @@ const flattenTwo = function(array, arr){
   return newArray;
 }
 
-//flattenTwo([1, 2, [3, 4], 5, [6]]);
-
-// Flatten function array.flat()
-/*const flatten = function(array) {
-let newArray = array.flat();
-return newArray
-}*/
-
-assertArraysEqual(flattenTwo([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+module.exports = flattenTwo;
