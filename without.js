f8ba22aef0cf37022e9dbb5b16cd317d84fd44e2
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 23:35:08 -0700

    eslint code

--- without.js
+++ without.js
@@ -1,12 +1,12 @@
-const without = function(array, value) {
-  newArray = []
-  array.forEach(function(element) {
-   if (value.includes(element)) {
-   } else { 
-     newArray.push(element)
-   }
- })
- return newArray
-}
+const without = function(array, value) {
+  let newArray = [];
+  array.forEach(function(element) {
+    if (value.includes(element)) {
+    } else {
+      newArray.push(element);
+    }
+  });
+  return newArray;
+};
 
 module.exports = without;
