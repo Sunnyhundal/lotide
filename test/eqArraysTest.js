5f7a9e621a945273f100c2a02f0d2cf758b3586e
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 21:35:17 -0700

    Created test for eqArrays using mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,9 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+describe("#eqArrays", () => {
+
+  it("true if arrays passed into eqArrays are equal" , () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+})
