8d4de904a1dd02a01cc3887163ae0baad20a1515
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 20:12:13 -0700

    removed test cases and set up export

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
@@ -21,7 +13,6 @@ const countOnly = function(allItems, itemsToCount) {
    results[item] = 1;
    }
     
-    console.log(item);
   }
   
   
@@ -31,23 +22,5 @@ const countOnly = function(allItems, itemsToCount) {
 };
 
 
-
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+//Export countOnly
+module.exports = countOnly;
