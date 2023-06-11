a2fd77ee59bfc4dfd05d4cf8cb096d7034293a62
Sunny Hundal <amandip.h@gmail.com>
2023-06-11 02:13:40 -0700

    Added comments about function

--- findKey.js
+++ findKey.js
@@ -1,3 +1,4 @@
+//Function finds a key value from an object based on the callback.
 const findKey = function(object, callback) {
   for (let key in object) {
     if (callback(object[key])) {
@@ -8,5 +9,5 @@ const findKey = function(object, callback) {
 };
 
 
-
+//export find ley for use outside file
 module.exports = findKey;
