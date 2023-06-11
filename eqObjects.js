67afad8bc7c6dcf7e25b01f18519564e23a47434
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 21:48:58 -0700

    Imported eqArrays and removed assertEquals and test cases

--- eqObjects.js
+++ eqObjects.js
@@ -1,21 +1,23 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let arrayCounter = 0;
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
+const eqArrays = require('./eqArrays');
+
+// const eqArrays = function(arrayOne, arrayTwo) {
+//   let arrayCounter = 0;
+//   if (arrayOne.length !== arrayTwo.length) {
+//     return false;
+//   }
   
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] === arrayTwo[i]) {
-      arrayCounter ++;
-    }
-  }
+//   for (let i = 0; i < arrayOne.length; i++) {
+//     if (arrayOne[i] === arrayTwo[i]) {
+//       arrayCounter ++;
+//     }
+//   }
   
-  if (arrayCounter !== arrayOne.length) {
-    return false;
-  } else {
-    return true;
-  }
-};
+//   if (arrayCounter !== arrayOne.length) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// };
 
 const assertEqual = function(actual, expected) {
   
@@ -49,11 +51,4 @@ console.log(keysOne);
 
 };
 
-
-//test cases
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject , anotherShirtObject), true); // => true
-
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false); // => false
+module.exports = eqObjects;
