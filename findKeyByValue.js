c71f05e9be7379cf694b431974d8346d96930975
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 22:41:13 -0700

    Removed test data

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,23 +1,16 @@
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
 function findKeyByValue(category, nameOfShow) {
   const keys = Object.keys(category);
-for (let show of keys) {
-//used for degugging
-  console.log(`${show} => ${category[show]}`)
+  for (let show of keys) {
+    //used for degugging
+    console.log(`${show} => ${category[show]}`);
 
-  if(category[show] === nameOfShow){
-    return show;
+    if (category[show] === nameOfShow) {
+      return show;
+    }
+  
+  
   }
-  
-  
-}
-return undefined;
+  return undefined;
 }
 
 module.exports = findKeyByValue;
