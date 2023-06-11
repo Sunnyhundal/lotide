6198c0af1c59cbc1e4918e86668c186c3f101657
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 23:12:55 -0700

    Created test for map using mocha and chai

---
+++ test/mapTest.js
@@ -0,0 +1,35 @@
+const assert = require('chai').assert;
+const map   = require('../map');
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+describe("#map", () => {
+  it("returns [0] index string and the first letter in ground", () => {
+    assert.strictEqual(results1[0], "g");
+  });
+  it("returns [1] index string and the first letter in control", () => {
+    assert.strictEqual(results2[1], "c");
+  });
+  it("returns last index string and the first letter in ground", () => {
+    assert.strictEqual(results4[0], "d");
+  })
+});
+
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word[0]);
+const results3 = map(words, word => word[Math.floor(word.length/2)]);
+const results4 = map(words, word => word[word.length - 1]);
+
+
+
+// console.log(results1);
+// console.log(results2);
+// console.log(results3);
+// console.log(results4);
+
+// assertArraysEqual(results1[0], "g");
+// assertArraysEqual(results2[1], "c");
+// assertArraysEqual(results3[0], "u");
+// assertArraysEqual(results4[0], "d");
