42a78cbd3033af4ce6cbd1c9d1224bb6089cca9d
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 23:11:47 -0700

    removed eqArray and assertArraysEqual

--- map.js
+++ map.js
@@ -1,30 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let arrayCounter = 0;
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] === arrayTwo[i]) {
-      arrayCounter ++;
-    }
-  }
-  
-  if (arrayCounter !== arrayOne.length) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${arrayOne} !== ${arrayTwo}`);
-  }
-};
-
 const words = ["ground", "control", "to", "major", "tom"];
 
 // const results1 = words.map(word => words[0]);
