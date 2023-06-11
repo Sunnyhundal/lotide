744cba6a9b3531358121bd9945fb17486e0ce3c1
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 23:23:43 -0700

    eslint code

--- flatten.js
+++ flatten.js
@@ -1,16 +1,16 @@
-const flattenTwo = function(array, arr){
-  newArray =[]
+const flattenTwo = function(array, arr) {
+  let newArray = [];
   for (let i = 0; i < array.length; i++) {
     
     if (Array.isArray(array[i])) {
       for (let index = 0; index < array[i].length; index++) {
         newArray.push(array[i][index]);
+      }
+    } else {
+      newArray.push(array[i]);
     }
-  }else{
-    newArray.push(array[i]);
-  }
   }
   return newArray;
-}
+};
 
 module.exports = flattenTwo;
