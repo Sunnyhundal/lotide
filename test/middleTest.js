aa14cb0545ab44e0b929c3fd9891a0ef1d6ec504
Sunny Hundal <amandip.h@gmail.com>
2023-06-11 00:48:58 -0700

    remove assertArraysEqual test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -12,11 +12,3 @@ describe("#middle", () => {
     assert.deepEqual(middle([1, 2]), []);
   });
 });
-
-// assertArraysEqual(middle([1]),[1]); //fail
-// assertArraysEqual(middle([1, 2]),[1.5]);//fail
-
-// assertArraysEqual(middle([1, 2, 3]),[2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
