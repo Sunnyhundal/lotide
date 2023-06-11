62e62a0fa5687a9bb693c23b295e23211ef80184
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 22:56:52 -0700

    removed eqArray, assertArrayEqual, and pseudo code

--- letterPositions.js
+++ letterPositions.js
@@ -1,30 +1,3 @@
-// const eqArrays = function(arrayOne, arrayTwo) {
-//   let arrayCounter = 0;
-//   if (arrayOne.length !== arrayTwo.length) {
-//     return false;
-//   }
-  
-//   for (let i = 0; i < arrayOne.length; i++) {
-//     if (arrayOne[i] === arrayTwo[i]) {
-//       arrayCounter ++;
-//     }
-//   }
-  
-//   if (arrayCounter !== arrayOne.length) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-
-// const assertArraysEqual = function(arrayOne, arrayTwo) {
-//   if (eqArrays(arrayOne, arrayTwo)) {
-//     console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
-//   } else {
-//     console.log(`🛑  Assertion failed: ${arrayOne} !== ${arrayTwo}`);
-//   }
-// };
-
 const letterPositions = function(sentence, letter) {
   const result = {};
   let letters = [];
@@ -48,13 +21,5 @@ const letterPositions = function(sentence, letter) {
 
   return result;
 };
-//let sentence = "lighthouse in the house";
-//let string = sentence.split(' ');
-//let test = Array.from(sentence)
-//create if-else statement to push index results
-
-
-//console.log(letterPositions("lighthouse in the house"));
-
 
 module.exports = letterPositions;
