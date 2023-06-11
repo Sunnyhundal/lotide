3e28d7fb806f8b54736f91a37ac28d0a2b119b99
Sunny Hundal <amandip.h@gmail.com>
2023-06-11 01:21:53 -0700

    Added comments describing how eqArrays works

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,17 @@
+// Function does a deeper look at two arrays
 const eqArrays = function(arrayOne, arrayTwo) {
   let arrayCounter = 0;
+  //Compares if two arrays have a similar length and if they don't a false value is returned
   if (arrayOne.length !== arrayTwo.length) {
     return false;
   }
-  
+  //If the two arrays ahve matching values a counter is updated.
   for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] === arrayTwo[i]) {
       arrayCounter ++;
     }
   }
-  
+  // If the arrayCounter does not match the  length of arrayOne then a false value is returned.If they match true is returned.
   if (arrayCounter !== arrayOne.length) {
     return false;
   } else {
@@ -17,5 +19,6 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
 };
 
+//export eqArrays for use outside of file
 module.exports = eqArrays;
 
