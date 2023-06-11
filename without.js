b7a462ce3af0084c5329fc8f1093186ed5179c54
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 23:32:38 -0700

    removed eqArray, assertArray, and test code

--- without.js
+++ without.js
@@ -1,30 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let arrayCounter = 0;
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] === arrayTwo[i]) {
-      arrayCounter ++;
-    }
-  }
-  
-  if (arrayCounter !== arrayOne.length) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${arrayOne} !== ${arrayTwo}`);
-  }
-};
-
 const without = function(array, value) {
   newArray = []
   array.forEach(function(element) {
@@ -36,9 +9,4 @@ const without = function(array, value) {
  return newArray
 }
 
-console.log(without(["1", "2", "3"], [1, 2, "3"]));
-console.log(without([1, 2, 3], [1]));
-const words = ["hello", "world", "lighthouse"];
-console.log(without(["hello", "world", "lighthouse"], ["lighthouse"]));
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(without([1, 2, 3], [3]), [1, 2])
+module.exports = without;
