cb961090afdd21128f23d70f35ab3923f6bf311c
Sunny Hundal <amandip.h@gmail.com>
2023-06-11 01:12:08 -0700

    Added comment describing function

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,9 @@
-
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
+//Function designed to return allitems that have been selected to be counted (true or false)
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-  
+
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
