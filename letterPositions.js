6109637ed5c7446a16f0f3f9b942571cb912cee9
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 22:52:04 -0700

    Checked with eslint and corrected

--- letterPositions.js
+++ letterPositions.js
@@ -1,53 +1,53 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let arrayCounter = 0;
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
+// const eqArrays = function(arrayOne, arrayTwo) {
+//   let arrayCounter = 0;
+//   if (arrayOne.length !== arrayTwo.length) {
+//     return false;
+//   }
   
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] === arrayTwo[i]) {
-      arrayCounter ++;
+//   for (let i = 0; i < arrayOne.length; i++) {
+//     if (arrayOne[i] === arrayTwo[i]) {
+//       arrayCounter ++;
+//     }
+//   }
+  
+//   if (arrayCounter !== arrayOne.length) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// };
+
+// const assertArraysEqual = function(arrayOne, arrayTwo) {
+//   if (eqArrays(arrayOne, arrayTwo)) {
+//     console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
+//   } else {
+//     console.log(`🛑  Assertion failed: ${arrayOne} !== ${arrayTwo}`);
+//   }
+// };
+
+const letterPositions = function(sentence, letter) {
+  const result = {};
+  let letters = [];
+  for (let index = 0; index < sentence.length; index++) {
+  
+    if (sentence[index] !== " ") {
+      letters.push(sentence[index]);
+    } else {
+      letters.push[index];
+      continue;
     }
-  }
   
-  if (arrayCounter !== arrayOne.length) {
-    return false;
-  } else {
-    return true;
+    if (!result[sentence[index]]) {
+      result[sentence[index]] = [];
+      result[sentence[index]].push(index);
+    } else {
+      result[sentence[index]].push(index);
+    }
+
   }
+
+  return result;
 };
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${arrayOne} !== ${arrayTwo}`);
-  }
-};
-
-const letterPositions = function(sentence, letter){
-const result = {};
-let letters =[];
-for (let index = 0; index < sentence.length; index++) {
-  
-  if (sentence[index] !== " ") {
-  letters.push(sentence[index]);
-  }else{
-  letters.push[index];
-  continue;
-  }
-  
-  if(!result[sentence[index]]) {
-    result[sentence[index]] = [];
-    result[sentence[index]].push(index);
-  } else {
-    result[sentence[index]].push(index);
-  }
-
-}
-
-return result;
-}
 //let sentence = "lighthouse in the house";
 //let string = sentence.split(' ');
 //let test = Array.from(sentence)
@@ -56,5 +56,5 @@ return result;
 
 //console.log(letterPositions("lighthouse in the house"));
 
-//test case
-assertArraysEqual(letterPositions("hello").e, [1]);
+
+module.exports = letterPositions;
