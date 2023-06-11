204ecb1237a7e4a2ea0898379f576c2f964fbbad
Sunny Hundal <amandip.h@gmail.com>
2023-06-11 00:40:13 -0700

    remove eqArray and assertArrayEqual code. Added description of function

--- middle.js
+++ middle.js
@@ -1,31 +1,4 @@
-// const eqArrays = function(arrayOne, arrayTwo) {
-//   let arrayCounter = 0;
-//   if (arrayOne.length !== arrayTwo.length) {
-//     return false;
-//   }
-  
-//   for (let i = 0; i < arrayOne.length; i++) {
-//     if (arrayOne[i] === arrayTwo[i]) {
-//       arrayCounter ++;
-//     }
-//   }
-  
-//   if (arrayCounter !== arrayOne.length) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-//const eqArrays = require('../eqArrays');
-//const assertArraysEqual = require('../assertArraysEqual');
-// const assertArraysEqual = function(arrayOne, arrayTwo) {
-//   if (eqArrays(arrayOne, arrayTwo)) {
-//     console.log(`✅  Assertion passed: ${arrayOne} === ${arrayTwo}`);
-//   } else {
-//     console.log(`🛑  Assertion failed: ${arrayOne} !== ${arrayTwo}`);
-//   }
-// };
-
+// Function returns the middle value of an array.
 const middle = function(array) {
   let middleArray = [];
   let middleValue = 0;
@@ -34,14 +7,14 @@ const middle = function(array) {
     return middleArray;
   
   }
-  //how to handle arrays that are even
+  //How to handle arrays that are even
   if (array.length % 2 === 0) {
     middleValue = array.length / 2;
     middleArray.push(middleValue);
     middleArray.push(middleValue + 1);
     return middleArray;
     
-    // how to process everything else (odd)
+    // How to process everything else (odd)
   } else {
     middleValue = array.length / 2;
     middleArray.push(array[Math.floor(middleValue)]);
