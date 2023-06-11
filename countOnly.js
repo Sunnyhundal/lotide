5e94eb9aa3e75a2599231b9209eb14d3ac848a9e
Sunny Hundal <amandip.h@gmail.com>
2023-06-10 20:46:05 -0700

    fixed missing logic(if statement) and removed asserEqual

--- countOnly.js
+++ countOnly.js
@@ -3,19 +3,17 @@ const assertEqual = require('./assertEqual');
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  const results = {}
+  const results = {};
   
   for (const item of allItems) {
-    
-   if (results[item]) {
-   results[item] += 1;
-   } else {
-   results[item] = 1;
-   }
-    
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
   }
-  
-  
   
   return results;
 
