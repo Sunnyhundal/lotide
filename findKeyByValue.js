e58504f9ee5f432d3274daff9b6cc07218d3f592
Sunny Hundal <amandip.h@gmail.com>
2023-06-14 13:28:01 -0700

    Remove debugging code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,9 +1,7 @@
 function findKeyByValue(category, nameOfShow) {
   const keys = Object.keys(category);
   for (let show of keys) {
-    //used for degugging
-    console.log(`${show} => ${category[show]}`);
-
+ 
     if (category[show] === nameOfShow) {
       return show;
     }
