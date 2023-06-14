f0b0228d1f80bab5f1d6290d0a71612461748d11
Sunny Hundal <amandip.h@gmail.com>
2023-06-14 11:27:52 -0700

    Remove test code

--- map.js
+++ map.js
@@ -1,8 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
-// const results1 = words.map(word => words[0]);
-// console.log(results1);
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
