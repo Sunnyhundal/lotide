f7aa2248a42d5ecc7c685ef27aeb7337e2bdafc7
Sunny Hundal <amandip.h@gmail.com>
2023-06-14 12:56:27 -0700

    Recator and comment code

--- without.js
+++ without.js
@@ -1,10 +1,11 @@
+// Function retuns an array with marked value(s) removed.
 const without = function(array, value) {
   let newArray = [];
   array.forEach(function(element) {
-    if (value.includes(element)) {
-    } else {
+    if (!value.includes(element)) {
       newArray.push(element);
     }
+    
   });
   return newArray;
 };
