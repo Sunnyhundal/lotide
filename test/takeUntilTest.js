5e8404dfe2ddc7ceaf187f5c39358cbfeb75b1db
Sunny Hundal <amandip.h@gmail.com>
2023-06-14 21:19:58 -0700

    Fix missing brackets after test code removal

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -5,4 +5,5 @@ const takeUntil   = require('../takeUntil');
 describe("#takeUntill", () => {
   it("returns [ 1, 2, 5, 7, 2 ] for takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0 )", () => {
     assert.deepEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0), [ 1, 2, 5, 7, 2 ]);
+  })
   });
