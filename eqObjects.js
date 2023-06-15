094af6f99973333e49d36acb63b40d88a599eb3b
Sunny Hundal <amandip.h@gmail.com>
2023-06-14 20:23:01 -0700

    Fix logic and remove test code

--- eqObjects.js
+++ eqObjects.js
@@ -1,24 +1,30 @@
 //Import eqArrays
 const eqArrays = require('./eqArrays');
 // A function designed to compare two objects
+
 const eqObjects = function(object1, object2) {
   const keysOne = Object.keys(object1);
   const keysTwo = Object.keys(object2);
-  console.log(keysOne);
+  
   // returns false if the two arrays do not have matching legnths
   if (keysOne.length !== keysTwo.length) {
     return false;
   }
 
   for (const keys of keysOne) {
-    if (eqArrays(object1[keys], object2[keys])) {
-      return true;
-    } else {
-      return false;
+    if(Array.isArray(object1[keys] && Array.isArray(object2[keys])))
+      if (!eqArrays(object1[keys], object2[keys])) {
+       return false;
     }
-  }
+     if (object1[keys] !== object2[keys]) {
+      return false
+     }
+    } 
+    return true;
+  };
+
   
 
-};
+
 //Export eqObjects for use outside file
 module.exports = eqObjects;
